7742de6406f206bb1ee8fdcfabb0e235f53d3f12
Cardi DeMonaco Jr <cardi@rokpte.com>
2019-04-19 11:24:32 -0400

    Implemented Edit page

---
+++ shopping-list/Pages/Manage/Initiatives/Edit.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using shopping_list.DataLayer;
+
+namespace shopping_list.WebApp.Pages.Manage.Initiatives
+{
+    public class EditModel : PageModel
+    {
+        private Shopping_listDataContext _context = new Shopping_listDataContext();
+
+        [BindProperty]
+        public Initiative Initiative { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Initiative = await _context.Initiatives.FindAsync(id);
+
+            if (Initiative == null)
+                return NotFound();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var initiativeToUpdate = await _context.Initiatives.FindAsync(id);
+
+            initiativeToUpdate.InitiativeName = Initiative.InitiativeName;
+            initiativeToUpdate.InitiativeDescription = Initiative.InitiativeDescription;
+
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+    }
+}
